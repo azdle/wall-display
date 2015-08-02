ea91d1238a4c301e59b57c039dda571ab3a2f1a7
Patrick Barrett <pat@theunlink.com>
2015-08-02 16:28:08 -0500

    beginnings of a real app

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserify = require('gulp-browserify');
+
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('js', function() {
+	return gulp.src('js/*.js')
+		.pipe(browserify())
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('public/js'))
+		.pipe(rename('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('css/*.css')
+		.pipe(gulp.dest('public/css'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('*.html')
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'js']);
+	gulp.watch('css/*.css', ['css']);
+	gulp.watch('*.html', ['html']);
+});
+
+gulp.task('default', ['lint', 'js', 'html', 'css'])
---
+++ js/main.js
@@ -0,0 +1,38 @@
+var moment = require('moment');
+
+var timeDiv, dateDiv, weatherDiv;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	timeDiv = document.getElementById('time');
+	dateDiv = document.getElementById('date');
+	weatherDiv = document.getElementById('weather');
+
+	updateTime();
+	window.setInterval(updateTime, 1000);
+
+	updateDate();
+	window.setInterval(updateDate, 10 * 1000);
+
+	updateWeather();
+	window.setInterval(updateWeather, 15 * 60 * 1000);
+});
+
+function updateTime(){
+	var d = new Date();
+	var dateStr = moment().format('HH:mm');
+	if (timeDiv.innerHTML != dateStr) {
+		timeDiv.innerHTML = dateStr;
+	}
+}
+
+function updateDate(){
+	var d = new Date();
+	var dateStr = moment().format('MMMM Do YYYY');
+	if (dateDiv.innerHTML != dateStr) {
+		dateDiv.innerHTML = dateStr;
+	}
+}
+
+function updateWeather(){
+	weatherDiv.innerHTML = "It Gonn' Rain!";
+}
