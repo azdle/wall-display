50d491b2bd2157e2953c46cd0da4cbc1b6c7d937
Patrick Barrett <pat@theunlink.com>
2015-08-03 20:29:03 -0500

    nevermind that didn't help

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://api.openweathermap.org')
   res.render('index', { title: 'Express' });
 });
 
