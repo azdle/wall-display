bdffee9fd59609afb4c49373c6694c97304b757f
Your Name <you@example.com>
2015-10-10 14:23:54 -0400

    add API key

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
   var q = req.query.q || "minneapolis,us";
   var units = req.query.units || "metric";
 
-  http.get("http://api.openweathermap.org/data/2.5/weather?q=" + q + ",us&units=" + units, function(wres) {
+  http.get("http://api.openweathermap.org/data/2.5/weather?APPID=77b8262890b262a44778a1a01a6b7cb2&q=" + q + ",us&units=" + units, function(wres) {
    var data = "";
    wres.on('data', function(chunk) {
     data += chunk.toString();
